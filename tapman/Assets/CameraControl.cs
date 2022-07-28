fea8a73d4a13830a5b19665072535cb086b8c0dc
JUSTKEVINSR <kevin.sr@students.amikom.ac.id>
2022-07-28 22:07:22 +0700

    Add camera control animation scrip
    - add Camera Control Scrip
    - add Animation Scrip (animation is currently not working due to devolopers  hardware limits)

---
+++ tapman/Assets/CameraControl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+     [SerializeField] private Transform Player;
+    // Start is called before the first frame update
+    
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = new Vector3(Player.position.x, Player.position.y, transform.position.z);
+    }
+}
--- tapman/Assets/PlayerControl.cs
+++ tapman/Assets/PlayerControl.cs
@@ -5,25 +5,53 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float dirX = 0f;
+
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 14f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
+     
+        UpdateAnimationState();
+    }
 
+    private void UpdateAnimationState()
+    {
+         if (dirX > 0f)
+        {
+            //anim.SetBool(" ", true);
+            sprite.flipX = false;
+        }
+        else if (dirX > 0f)
+        {
+            //anim.SetBool(" ", false);
+            sprite.flipX = true;
+        }
+        else
+        {
+        //anim.SetBool(" ", false);
+        }
     }
 }
