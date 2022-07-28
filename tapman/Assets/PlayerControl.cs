e7906e7c2f8216775c91bfb97cd5f5aabc589e3d
JUSTKEVINSR <kevin.sr@students.amikom.ac.id>
2022-07-28 23:34:23 +0700

    Add Ground Check
    - Add Ground Check

--- tapman/Assets/PlayerControl.cs
+++ tapman/Assets/PlayerControl.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anim;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     private float dirX = 0f;
 
@@ -19,6 +22,7 @@ public class PlayerControl : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
@@ -29,7 +33,7 @@ public class PlayerControl : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -67,5 +71,10 @@ public class PlayerControl : MonoBehaviour
         }
 
         //anim.SetInteger("state", (int) state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
